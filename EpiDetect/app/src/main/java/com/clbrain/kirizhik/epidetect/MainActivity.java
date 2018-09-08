62f8625a1b921b57bc68bc1805bc2330e958af6b
olpyhn <olpyhn@gmail.com>
2018-09-08 10:12:00 +0300

    Login

--- EpiDetect/app/src/main/java/com/clbrain/kirizhik/epidetect/MainActivity.java
+++ EpiDetect/app/src/main/java/com/clbrain/kirizhik/epidetect/MainActivity.java
@@ -1,7 +1,10 @@
 package com.clbrain.kirizhik.epidetect;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.btn_main_red);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
     }
 }
