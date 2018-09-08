ee5042592b9b9ccb1a94c29536842e910cbfe9ae
olpyhn <olpyhn@gmail.com>
2018-09-08 17:36:02 +0300

    v.1

--- EpiDetect/app/src/main/java/com/clbrain/kirizhik/epidetect/MainActivity.java
+++ EpiDetect/app/src/main/java/com/clbrain/kirizhik/epidetect/MainActivity.java
@@ -12,12 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn = findViewById(R.id.btn_main_red);
-        btn.setOnClickListener(new View.OnClickListener() {
+        Button connect = findViewById(R.id.btn_main_red);
+        connect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                startActivity(new Intent(getApplicationContext(), ConnectActivity.class));
             }
         });
+
+
     }
 }
