589abb1c02c9ddd1739f800b00ea1a9793be7e72
olpyhn <olpyhn@gmail.com>
2018-09-09 10:26:01 +0300

    small changes

--- EpiDetect/app/src/main/java/com/clbrain/kirizhik/epidetect/RegistrationActivity.java
+++ EpiDetect/app/src/main/java/com/clbrain/kirizhik/epidetect/RegistrationActivity.java
@@ -15,8 +15,10 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
-    Button sign_in;
-    Button sign_up;
+    Button signIn;
+    Button signUp;
+    Button men;
+    Button women;
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private EditText mName;
     private EditText mNum;
@@ -29,23 +31,37 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-        sign_up=findViewById(R.id.sign_up_btn);
-        sign_in=findViewById(R.id.sign_in_btn);
-        sign_in.setOnClickListener(new View.OnClickListener() {
+        signUp =findViewById(R.id.sign_up_btn);
+        signIn =findViewById(R.id.sign_in_btn);
+        signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(),LoginActivity.class));
             }
         });
-        sign_up.setOnClickListener(new View.OnClickListener() {
+        signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Регистрация и тп
                 register(mEmail.getText().toString(),mPassword.getText().toString());
-                //startActivity((new Intent(getApplicationContext(),MainActivity.class)));
             }
         });
-
+        men = findViewById(R.id.btn_men);
+        men.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                men.setSelected(true);
+                women.setSelected(false);
+            }
+        });
+        women = findViewById(R.id.btn_women);
+        women.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                women.setSelected(true);
+                men.setSelected(false);
+            }
+        });
     }
     private void register(String email, String password) {
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this,
@@ -54,6 +70,7 @@ public class RegistrationActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             Toast.makeText(RegistrationActivity.this, "Sign up is successful", Toast.LENGTH_SHORT).show();
+                            startActivity((new Intent(getApplicationContext(),MainActivity.class)));
                         }
                     }
                 });
