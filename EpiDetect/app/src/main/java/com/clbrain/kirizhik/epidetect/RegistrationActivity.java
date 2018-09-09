0fdc480aff3935cdaf80ce924754f27972d9e02c
KirillR911 <KirikR911@gmail.com>
2018-09-09 08:20:22 +0300

    New button change activity action

--- EpiDetect/app/src/main/java/com/clbrain/kirizhik/epidetect/RegistrationActivity.java
+++ EpiDetect/app/src/main/java/com/clbrain/kirizhik/epidetect/RegistrationActivity.java
@@ -1,13 +1,27 @@
 package com.clbrain.kirizhik.epidetect;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegistrationActivity extends AppCompatActivity {
+    Button sign_in;
+    Button sign_up;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        sign_up=findViewById(R.id.sign_up_btn);
+        sign_in=findViewById(R.id.sign_in_btn);
+        sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+            }
+        });
+
     }
 }
